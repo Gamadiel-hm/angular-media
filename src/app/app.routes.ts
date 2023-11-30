efbe1aaaec4e8206d4258e6f2217e9ca85af28b8
Gamadiel <gamadiel-hm@hotmail.com>
2023-11-30 16:21:59 -0600

    Create componentsPage and router home children

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
+    path: '', // https://localhost:4200
+    loadChildren: () =>
+      import('./modules/home/homeRoutes').then((rout) => rout.Home_Routes),
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./modules/auth/auth.component').then(
+        (comp) => comp.AuthComponent
+      ),
   },
 ];
---
+++ src/app/modules/home/homeRoutes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+export const Home_Routes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
