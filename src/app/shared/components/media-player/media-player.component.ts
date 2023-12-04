33e1ebc31fc2cae2a1163faf55e3192d5e982db5
Gamadiel <gamadiel-hm@hotmail.com>
2023-12-03 23:31:41 -0600

    finished mediaPaler UI

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,12 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-media-player',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './media-player.component.html',
-  styleUrl: './media-player.component.css'
+  styleUrl: './media-player.component.css',
 })
-export class MediaPlayerComponent {
-
-}
+export class MediaPlayerComponent {}
