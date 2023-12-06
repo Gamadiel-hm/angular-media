6f20a3f21e6f3479fb40ae525b41899c8e3c3327
Gamadiel <gamadiel-hm@hotmail.com>
2023-12-05 21:36:54 -0600

    Update media tracks

--- src/app/modules/favorites/favorites-page.component.ts
+++ src/app/modules/favorites/favorites-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { PlayListBodyComponent } from '@share/components/play-list-body/play-list-body.component';
+import { PlayListHeaderComponent } from '@share/components/play-list-header/play-list-header.component';
 
 @Component({
   selector: 'app-favorites-page',
   standalone: true,
-  imports: [],
+  imports: [PlayListBodyComponent, PlayListHeaderComponent],
   templateUrl: './favorites-page.component.html',
-  styleUrl: './favorites-page.component.css'
+  styleUrl: './favorites-page.component.css',
 })
-export class FavoritesPageComponent {
-
-}
+export class FavoritesPageComponent {}
